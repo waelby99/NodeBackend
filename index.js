cd50491e21fde4ffb0bdf2fbd686209ba9d05523
waelby99 <waelbenyoussef19991@gmail.com>
2025-03-14 18:36:46 +0100

    installing and configuring nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req, res){
-    res.send('hello')
+    res.send('hello biatch')
 })
 
 app.listen(3000,()=>console.log("Server is running !"))
