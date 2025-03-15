c46b54526b260172aeeffa44156275dd9338390b
waelby99 <waelbenyoussef19991@gmail.com>
2025-03-15 22:52:59 +0100

    ..

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req, res){
-    res.send('hello biatch')
+    res.send('hello bitch')
 })
 
 app.listen(3000,()=>console.log("Server is running !"))
